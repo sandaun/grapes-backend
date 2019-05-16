4a7bad6996146ac61763efef3a60931939698a3f
Oriol Carbo <oriol.carbo@gmail.com>
2019-05-17 00:09:52 +0200

    UPDATES profile.js with updated /update method updating req.session in lines 28 & 29

--- routes/profile.js
+++ routes/profile.js
@@ -7,6 +7,17 @@ const {
   isLoggedIn,
 } = require('../helpers/middlewares');
 
+router.get('/', (req, res, next) => {
+  const _id  = req.session.currentUser._id;
+
+  User.findById({ _id })
+    .then((user) => {
+      res.status(200);
+      res.json(user);
+    })
+    .catch(next);
+});
+
 router.post('/update', isLoggedIn(), (req, res, next) => {
   const {
     username,
@@ -14,9 +25,12 @@ router.post('/update', isLoggedIn(), (req, res, next) => {
   } = req.body;
 
   const currentUser = req.session.currentUser._id;
+  req.session.currentUser.username = username;
+  req.session.currentUser.email = email;
 
   User.findOneAndUpdate({ _id: currentUser }, { username, email }, { new: true })
     .then((user) => {
+      console.log('user desde back!')
       res.status(200);
       res.json(user);
     })
