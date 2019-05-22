05e9086009e2322a8027c72ce3017cfb2cce696f
Oriol Carbo <oriol.carbo@gmail.com>
2019-05-22 20:51:35 +0200

    ADDS add and remove recipes in user model array. Also recipes array can be listed

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,6 +4,15 @@ const axios = require('axios');
 
 const router = express.Router();
 
+const User = require('../models/user');
+
+const {
+  isLoggedIn,
+} = require('../helpers/middlewares');
+
+// const currentUser = req.session.currentUser._id;
+//   req.session.currentUser.favoriteRecipes = favoriteRecipes;
+
 const getFullRecipeSelected= axios.create({
   baseURL: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/',
   headers: {
@@ -24,5 +33,50 @@ router.get('/recipelist', async (req, res) => {
       message: 'Request to Spoonacular failed/unauthorized'
     }));
 });
+
+
+// GET FAVORITE RECIPES BY USER
+router.get('/favorite', isLoggedIn(), (req, res, next) => {
+  const userID = req.session.currentUser._id;
+  User.findById(userID)
+  .then(favoriteList => {
+    req.session.currentUser.favoriteRecipes = favoriteList.favoriteRecipes
+    res.status(200)
+    res.json(favoriteList.favoriteRecipes)
+  })
+  .catch(err =>{
+    res.json(err);
+  })
+});
+
+// ADD FAVORITE
+router.put('/favorite/:id', isLoggedIn(),  (req, res, next) => {
+  const userID = req.session.currentUser._id;
+  const { id } = req.params;
+  console.log(id, 'this is favorites');
+  User.findByIdAndUpdate( userID, 
+    {$push:{ favoriteRecipes: id }}, {new:true})
+    .then(recipesArray =>{
+    console.log('RECIPES UPDATED IN BBDD', recipesArray)
+    res.json(recipesArray.favoriteRecipes)
+    })
+    .catch(err =>{
+      res.json(err)
+    })
+  });
+
+//DELETE FAVORITE
+  router.post('/favorite/delete/:id', isLoggedIn(), async (req, res, next) => {
+   try {
+    const userID = req.session.currentUser;
+    const { id } = req.params;
+    console.log('fav',req.session.currentUser.favoriteRecipes)
+    let deleteFavorite = await User.findByIdAndUpdate( userID, { $pull: { favoriteRecipes: id}, new: true})
+      res.json(deleteFavorite)
+    }
+    catch(err){
+      res.json(err);
+    }
+  });
 
 module.exports = router;
