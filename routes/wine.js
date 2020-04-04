44431ecf043f22bb5e63b10f970906e2bf82ab1a
Oriol Carbo <oriol.carbo@gmail.com>
2020-04-05 01:54:17 +0200

    Updates wine.js with new spoonacular api url. Still foodlist route to be fixed from getImageFromFoodPaired

--- routes/wine.js
+++ routes/wine.js
@@ -5,42 +5,45 @@ const axios = require('axios');
 const router = express.Router();
 
 const getFoodByPairingWine = axios.create({
-  baseURL: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/',
-  headers: {
-    // 'Accept': 'application/json',
-    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.SPOON || 'getyourownapikeyatmashape'
-  }
+  baseURL: 'https://api.spoonacular.com/food/wine/',
+  // headers: {
+  //   // 'Accept': 'application/json',
+  //   'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+  //   'X-RapidAPI-Key': process.env.SPOON || 'getyourownapikeyatmashape'
+  // }
 });
 
 const getImageFromFoodPaired = axios.create({
-  baseURL: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/menuItems/',
+  baseURL:
+    'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/menuItems/',
   headers: {
     // 'Accept': 'application/json',
     'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.SPOON || 'getyourownapikeyatmashape'
-  }
+    'X-RapidAPI-Key': process.env.SPOON || 'getyourownapikeyatmashape',
+  },
 });
 
 const getEdamam = axios.create({
   baseURL: 'https://api.edamam.com/',
 });
 
-
 router.get('/foodlist', async (req, res) => {
   const { wine } = req.query;
-  const  dataFoodToFront = {
+  const dataFoodToFront = {
     pairedFoodwithWine: [],
     foodUrlArray: [],
-  }
-  await getFoodByPairingWine.get(`dishes?wine=${wine}`)
-    .then((food) => {
+  };
+  await getFoodByPairingWine
+    .get(`dishes?apiKey=89d2edec85fb4a5db2f20c60f72cff3b&wine=${wine}`)
+    .then(food => {
       dataFoodToFront.pairedFoodwithWine = food.data.pairings;
     })
-    .catch(e => res.status(400).json({
-      message: 'Request to Spoonacular failed/unauthorized'
-    }));
-   
+    .catch(e =>
+      res.status(400).json({
+        message: 'Request to Spoonacular failed/unauthorized',
+      }),
+    );
+
   // for (let food of dataFoodToFront.pairedFoodwithWine) {
   //   await getEdamam.get(`search?q=${food}&app_id=${process.env.EDAMAM_ID}&app_key=${process.env.EDAMAM_KEY}`)
   //     .then((menu) => {
@@ -51,36 +54,42 @@ router.get('/foodlist', async (req, res) => {
   //     .catch(e => res.status(401).json({
   //       message: 'Request to Edamam failed/unauthorized'
   //     }));
-  //   }      
+  //   }
 
-    // The next part is commented as it is the original API and to be used if the above fails.
+  // The next part is commented as it is the original API and to be used if the above fails.
 
-    for (let food of dataFoodToFront.pairedFoodwithWine) {
-      await getImageFromFoodPaired.get(`search?number=10&query=${food}`)
-        .then((menu) => {
-          // Control if the menuItems array exists or not and give th default food image URL to frontend
-          if (menu.data.menuItems.length == 0 || menu.data.menuItems === undefined) {
-            const image = '/images/food/foodDefault.jpg'
-            dataFoodToFront.foodUrlArray.push(image)          
-          } else {
-            const { image } = menu.data.menuItems[0];
-            dataFoodToFront.foodUrlArray.push(image)
-          }
-        })
-      }   
+  for (let food of dataFoodToFront.pairedFoodwithWine) {
+    await getImageFromFoodPaired
+      .get(`search?number=10&query=${food}`)
+      .then(menu => {
+        // Control if the menuItems array exists or not and give th default food image URL to frontend
+        if (
+          menu.data.menuItems.length == 0 ||
+          menu.data.menuItems === undefined
+        ) {
+          const image = '/images/food/foodDefault.jpg';
+          dataFoodToFront.foodUrlArray.push(image);
+        } else {
+          const { image } = menu.data.menuItems[0];
+          dataFoodToFront.foodUrlArray.push(image);
+        }
+      });
+  }
   res.send(dataFoodToFront);
 });
 
-
 router.get('/winelist', (req, res) => {
   const { food } = req.query;
-  getFoodByPairingWine.get(`pairing?food=${food}`)
-    .then((wine) => {
-      res.send(wine.data)
+  getFoodByPairingWine
+    .get(`pairing?apiKey=89d2edec85fb4a5db2f20c60f72cff3b&food=${food}`)
+    .then(wine => {
+      res.send(wine.data);
     })
-    .catch(e => res.status(400).json({
-      message: 'Request to Spoonacular failed/unauthorized'
-    }));
+    .catch(e =>
+      res.status(400).json({
+        message: 'Request to Spoonacular failed/unauthorized',
+      }),
+    );
 });
 
 module.exports = router;
