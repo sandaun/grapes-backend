0c76e62b118fc397298e088f7797581828dfe796
Oriol Carbo <oriol.carbo@gmail.com>
2020-04-05 01:18:36 +0200

    New spoonacular URL added for recipies routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,83 +5,91 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-const {
-  isLoggedIn,
-} = require('../helpers/middlewares');
+const { isLoggedIn } = require('../helpers/middlewares');
 
-const getFullRecipeSelected= axios.create({
-  baseURL: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/',
-  headers: {
-    // 'Accept': 'application/json',
-    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.SPOON || 'getyourownapikeyatmashape'
-  }
+const getFullRecipeSelected = axios.create({
+  baseURL: 'https://api.spoonacular.com/recipes/',
+  // headers: {
+  //   // 'Accept': 'application/json',
+  //   'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+  //   'X-RapidAPI-Key': process.env.SPOON || 'getyourownapikeyatmashape',
+  // },
 });
 
 router.get('/recipelist', async (req, res) => {
   const { query } = req.query;
-  await getFullRecipeSelected.get(`search?query=${query}&number=5`)
-    .then((recipe) => {
-      res.send(recipe.data)
+  await getFullRecipeSelected
+    .get(`search?apiKey=89d2edec85fb4a5db2f20c60f72cff3b&query=${query}`)
+    .then(recipe => {
+      res.send(recipe.data);
     })
-    .catch(e => res.status(400).json({
-      message: 'Request to Spoonacular failed/unauthorized'
-    }));
+    .catch(e =>
+      res.status(400).json({
+        message: 'Request to Spoonacular failed/unauthorized',
+      }),
+    );
 });
 
 // GET RECIPE BY ID - TO BE IMPLEMENTED V2
 router.get('/recipelist/favorites', async (req, res) => {
   const { query } = req.query;
 
-  await getFullRecipeSelected.get(`${query}/information`)
-    .then((recipe) => {
-      res.send(recipe.data)
+  await getFullRecipeSelected
+    .get(`${query}/information`)
+    .then(recipe => {
+      res.send(recipe.data);
     })
-    .catch(e => res.status(400).json({
-      message: 'Request to Spoonacular failed/unauthorized'
-    }));   
+    .catch(e =>
+      res.status(400).json({
+        message: 'Request to Spoonacular failed/unauthorized',
+      }),
+    );
 });
-
 
 // GET FAVORITE RECIPES BY USER
 router.get('/favorite', isLoggedIn(), (req, res, next) => {
   const userID = req.session.currentUser._id;
   User.findById(userID)
-  .then(favoriteList => {
-    req.session.currentUser.favoriteRecipes = favoriteList.favoriteRecipes
-    res.status(200)
-    res.json(favoriteList.favoriteRecipes)
-  })
-  .catch(err =>{
-    res.json(err);
-  })
+    .then(favoriteList => {
+      req.session.currentUser.favoriteRecipes = favoriteList.favoriteRecipes;
+      res.status(200);
+      res.json(favoriteList.favoriteRecipes);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 // ADD FAVORITE
-router.put('/favorite/:id', isLoggedIn(),  (req, res, next) => {
+router.put('/favorite/:id', isLoggedIn(), (req, res, next) => {
   const userID = req.session.currentUser._id;
   const { id } = req.params;
-  User.findByIdAndUpdate( userID, 
-    {$push:{ favoriteRecipes: id }}, {new:true})
-    .then(recipesArray =>{
-    res.json(recipesArray.favoriteRecipes)
+  User.findByIdAndUpdate(
+    userID,
+    { $push: { favoriteRecipes: id } },
+    { new: true },
+  )
+    .then(recipesArray => {
+      res.json(recipesArray.favoriteRecipes);
     })
-    .catch(err =>{
-      res.json(err)
-    })
-  });
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 //DELETE FAVORITE
-  router.post('/favorite/delete/:id', isLoggedIn(), async (req, res, next) => {
-   try {
+router.post('/favorite/delete/:id', isLoggedIn(), async (req, res, next) => {
+  try {
     const userID = req.session.currentUser;
     const { id } = req.params;
-    let deleteFavorite = await User.findByIdAndUpdate( userID, { $pull: { favoriteRecipes: id}, new: true})
-      res.json(deleteFavorite)
-    }
-    catch(err){
-      res.json(err);
-    }
-  });
+    let deleteFavorite = await User.findByIdAndUpdate(userID, {
+      $pull: { favoriteRecipes: id },
+      new: true,
+    });
+    res.json(deleteFavorite);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 module.exports = router;
